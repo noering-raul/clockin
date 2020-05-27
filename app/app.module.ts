699891df866073cf9b4d13d0fd7b2bb1a53fdf70
raulnoering <45273284+raulnoering@users.noreply.github.com>
2020-05-26 21:21:51 -0300

    Auto-generated commit

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NovoComponenteComponent } from './novo-componente/novo-componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovoComponenteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
